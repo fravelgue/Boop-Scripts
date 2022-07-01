7631bc2f000f824c0869249c7414a0acbb504195
fravelgue <fravelgue@gmail.com>
2022-07-01 18:40:01 +0200

    Convert to title case

---
+++ Scripts/TitleCase.js
@@ -0,0 +1,38 @@
+/**
+  {
+    "api":1,
+    "name":"Convert selection to Title Case",
+    "description":"Convert to Title Case",
+    "author":"Kris Linquist",
+    "icon":"type",
+    "tags":"title,case"
+  }
+**/
+
+// https://raw.githubusercontent.com/IvanMathy/Boop/459249f931cef49961de715022e0aa4df662423c/Scripts/TitleCase.js
+function main(input) {
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const noCaps = ['a', 'an', 'the', 'for', 'and', 'nor', 'but', 'or', 'yet', 'so', 'at', 'around', 'by', 'after', 'along', 'for', 'from', 'of', 'on', 'to', 'with', 'without'];
+
+    const newString = [];
+    const words = input.text.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        //First and last
+        if (i == 0 || i + 1 == words.length) {
+            newString.push(capitalizeFirstLetter(words[i]));
+        } else {
+            if (noCaps.indexOf(words[i].toLowerCase()) > -1){
+                newString.push(words[i].toLowerCase());
+            } else {
+                newString.push(capitalizeFirstLetter(words[i]));
+            }
+        }
+    }
+
+    input.text = newString.join(' ');
+}
